75923a4faa703ec30da639ac1e615321ac686564
sergio960319 <42194152+sergio960319@users.noreply.github.com>
2018-08-23 23:33:58 -0500

    video 6

---
+++ Sales.Common/Models/Response.cs
@@ -0,0 +1,12 @@
+﻿namespace Sales.Common.Models
+{
+    public class Response
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public string Result { get; set; }
+
+
+
+    }
+}
--- Sales.Domain/Models/DataContext.cs
+++ Sales.Domain/Models/DataContext.cs
@@ -10,7 +10,7 @@
 
         }
 
-     
+        public System.Data.Entity.DbSet<Sales.Common.Models.Product> Products { get; set; }
     }
 
 }
